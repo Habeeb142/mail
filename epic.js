b4e2f06c62ced0611bc46eebdf5637190a525811
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2019-07-01 13:54:49 +0100

    Update epic.js

--- epic.js
+++ epic.js
@@ -38,6 +38,49 @@ epic.get('/', (req, res)=>{
 
 
 //signup:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-epic.get('/signup', (req,res)=>{
+epic.get('/sign_up', (req,res)=>{
      res.render('signup', {status: null, username: null, mobile: null});
 });
+
+//submitting sign-up data:::::::::::::::::::::::::::::::::::::::::::::::::
+epic.post('/signup', (req, res)=>{
+    //creating a method from formidable class::::::::::::::::::::::::::::::
+    var form = new fm.IncomingForm();
+    
+    //submitting files and fields to database:::::::::::::::::::::::
+    form.parse(req, (err, fields, files)=>{
+        if(fields.username !="" && fields.mobile !="" && fields.cpassword !="" && fields.password != ""){
+            //checking passowrd and confirm passowrd are equal::::::::::::::::::::
+            if(fields.cpassword == fields.password){
+                
+                let tmp = files.file.path;
+                let pix = files.file.name;
+                let img = pix;
+                let imgLink = "public/userImages/"+pix;
+
+                let newUserInfo = {
+                    username: fields.username,
+                    mobile: fields.mobile,
+                    password: fields.password,
+                    file: img
+                }
+        
+                //putting into database and sending from temporary location to permanent location::::::::::::::::::::::::::::::::::::
+                fs.rename(tmp, imgLink, ()=>{
+                    
+                    var nUser = new user(newUserInfo);
+                    nUser.save().then(data=>{
+                        res.render('index', {status: 'signedIn', username: fields.username, mobile: null})
+                    })
+                })
+            }
+        else{
+            res.render('signup', { status: 'password_err', username: fields.username, mobile: fields.mobile })
+        }
+    }
+    else{
+        res.render('signup', { status: 'fillform_err', username: fields.username, mobile: fields.mobile })
+    }     
+})
+})
+
