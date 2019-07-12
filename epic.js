7055e2e56037bb1d9260038f77cf762292f7aa76
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2019-07-12 15:09:59 +0100

    Update epic.js

--- epic.js
+++ epic.js
@@ -242,16 +242,12 @@ io.sockets.on('connection', (socket)=>{
        connection.query(sql_select, (err, dat, fields)=>{
             // user.find({ username: data.frndUsername }, (err, result)=>{
             //     user.find({ username: data.user }, (err, result1)=>{
-            sql_select1 = `SELECT username, mobile, file FROM user where username like
-            '${data.frndUsername}'`;
-connection.query(sql_select1, (err, result, fields)=>{
-            sql_select0 = `SELECT username, mobile, file FROM user where username like
-            '${data.user}'`;
+            
            
-                connection.query(sql_select0, (err, result1, fields)=>{
-                   socket.emit('old_message', { msg_prop: dat, frnd_img: result[0].file , frnd_img1: result1[0].file })
-                })
-            })
+              //  connection.query(sql_select0, (err, result1, fields)=>{
+                   socket.emit('old_message', { msg_prop: dat, frnd_img: null , frnd_img1: null })
+              //  })
+        //    })
          })
     })
 
